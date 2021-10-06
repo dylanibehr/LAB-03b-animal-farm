d1e95507a0db41d8f2974ccc06e883a20117c6fa
Dylan Behr <dylanibehr@gmail.com>
2021-10-06 15:59:51 -0600

    updated with sounds and images

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-// import functions
+const catimage = document.getElementById('cat-image');
+const catsound = document.getElementById('cat-sound');
+const dogimage = document.getElementById('dog-image');
+const dogsound = document.getElementById('dog-sound');
+const horseimage = document.getElementById('horse-image');
+const horsesound = document.getElementById("horse-sound");
 
-// reference needed DOM elements
+
+catimage.addEventListener('click' , () => {
+    console.log()
+    catsound.play();
+});
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'c') {
+        catsound.play();
+    }
+});
 
 
-// set event listeners 
-    // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+
+dogimage.addEventListener('click' , () => {
+    console.log()
+    dogsound.play();
+});
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'd') {
+        dogsound.play();
+    }
+});
+
+horseimage.addEventListener('click' , () => {
+    console.log()
+    horsesound.play();
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'h') {
+        horsesound.play();
+    }
+});
